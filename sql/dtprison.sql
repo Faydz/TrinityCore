e6e8fb2297e54aacc79491a71bd91cddf34704a5
Ishigure <ishi@m3nk3.eu>
2012-05-20 03:12:22 +0200

    Forgot something in the dtprison.sql

--- sql/dtprison.sql
+++ sql/dtprison.sql
@@ -1,7 +1,9 @@
 /* Add DTPrison Instance template entry & access requirements */
-INSERT INTO access_requirement (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `quest_failed_text`, `comment`) VALUES
+DELETE FROM `access_requirement` WHERE `mapId` = 35;
+INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `quest_failed_text`, `comment`) VALUES
 (35, 0, 80, 80, 0, 0, 0, 0, 0, NULL, 'DT-Ini: Das Verlies von Sturmwind'),
 (35, 2, 80, 80, 0, 0, 0, 0, 0, NULL, 'DT-Ini: Das Verlies von Sturmwind');
 
-INSERT INTO instance_template (`map`, `parent`, `script`, `allowMount`) VALUES
+DELETE FROM `instance_template` WHERE `map` = 35;
+INSERT INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES
 (35, 0, 'instance_dtprison', 0);
