c443635bda360c8ec800e78cd524fba1e835dcff
Thainification <thainification@gmx.de>
2012-08-30 22:11:44 +0200

    [Misc] Spell & remove one commit
    > King of the jungle and tiger's fury
    > revert 53db3d9700408c4053608e00370c0dfe5bce20fb & 65469c9ee916fd8a910ce39e94cc929b4e1487ea

--- sql/dt/Misc.sql
+++ sql/dt/Misc.sql
@@ -3,6 +3,7 @@ DELETE FROM spell_proc_event WHERE entry = 16164;
 INSERT INTO spell_proc_event (entry, SchoolMask, SpellFamilyName, SpellFamilyMask0, SpellFamilyMask1, SpellFamilyMask2, procFlags, procEx, ppmRate, CustomChance, Cooldown) VALUES (16164, 0, 11, 2416967683, 4096, 0, 0, 2, 0, 0, 0);
 
 -- Fix Dancing Rune Weapon.
+DELETE FROM `spell_proc_event` where `entry` = 49028;
 INSERT INTO `spell_proc_event` (entry, procFlags) VALUES
 ('49028','69652');
 
@@ -12,3 +13,6 @@ DELETE FROM spell_bonus_data WHERE entry = 33763;
 INSERT INTO spell_bonus_data (entry, direct_bonus, dot_bonus, ap_bonus, ap_dot_bonus, comments) VALUES
 (33778, 0, 0, 0, 0, 'Druid - Lifebloom final heal'),
 (33763, 0.516, 0.0952, 0, 0, 'Druid - Lifebloom HoT(rank 1)');
+
+-- King of the jungle and tiger's fury 
+UPDATE spell_proc_event SET procFlags = 1024 WHERE entry in (48492,48494,48495);
