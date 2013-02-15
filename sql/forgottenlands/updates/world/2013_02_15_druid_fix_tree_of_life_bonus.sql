c305214da53cafe41e74c2409834b03c4545d7ce
Link88 <pof1981@hotmail.it>
2013-02-16 00:05:04 +0100

    [Core-Db/Talents] Druid, Fix Tree of Life bonuses sql

---
+++ sql/forgottenlands/updates/world/2013_02_15_druid_fix_tree_of_life_bonus.sql
@@ -0,0 +1,2 @@
+INSERT INTO `spell_linked_spell` VALUES (-33891, -81097, 0, 'Druid: Tree of life bonuses remove');
+INSERT INTO `spell_linked_spell` VALUES (33891, 81097, 0, 'Druid: Tree of life bonuses');
