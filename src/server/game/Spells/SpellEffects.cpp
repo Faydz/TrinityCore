38ad3924649e025d67b701b3df2ee355b37c0ec3
Link88 <pof1981@hotmail.it>
2013-03-06 19:51:36 +0100

    [Core/Spells] Warrior, Fix Heroic Strike, Cleave damage calculation

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -443,15 +443,27 @@ void Spell::EffectSchoolDMG(SpellEffIndex effIndex)
                 break;
             case SPELLFAMILY_WARRIOR:
             {
-                // Victory Rush
-                if (m_spellInfo->Id == 34428)
-                    ApplyPct(damage, m_caster->GetTotalAttackPowerValue(BASE_ATTACK));
-                // Shockwave
-                else if (m_spellInfo->Id == 46968)
+                switch(m_spellInfo->Id)
                 {
-                    int32 pct = m_caster->CalculateSpellDamage(unitTarget, m_spellInfo, 2);
-                    if (pct > 0)
-                        damage += int32(CalculatePct(m_caster->GetTotalAttackPowerValue(BASE_ATTACK), pct));
+                    // Victory Rush
+                    case 34428:
+                        ApplyPct(damage, m_caster->GetTotalAttackPowerValue(BASE_ATTACK));
+                        break;
+                    // Heroic Strike
+                    case 78:
+                        damage = 8 + m_caster->GetTotalAttackPowerValue(BASE_ATTACK) * 0.6f;
+                        break;
+                    // Cleave
+                    case 845:
+                        damage += int32(6 + m_caster->GetTotalAttackPowerValue(BASE_ATTACK) * 0.45f);
+                        break;
+                    // Shockwave
+                    case 46968: 
+                    {
+                        int32 pct = m_caster->CalculateSpellDamage(unitTarget, m_spellInfo, EFFECT_2);
+                        if (pct > 0)
+                            damage += int32(CalculatePct(m_caster->GetTotalAttackPowerValue(BASE_ATTACK), pct));
+                    }
                     break;
                 }
                 break;
