22e2b1b94947105ecf527117b3f1d866b6aba83c
Link88 <pof1981@hotmail.it>
2013-04-05 00:23:42 +0200

    [Core/Spells] Warrior, fix Charge/Intercept miss

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3233,6 +3233,11 @@ void SpellMgr::LoadSpellCustomAttr()
             case SPELLFAMILY_WARRIOR:
                 switch(spellInfo->Id)
                 {
+                    // Charge/Intercept stun
+                    case 7922:
+                    case 20253:
+                        spellInfo->AttributesEx8 |= SPELL_ATTR8_CANT_MISS;
+                        break;
                     // Bloodsurge
                     case 46916:
                         spellInfo->Effects[EFFECT_2].Effect = SPELL_EFFECT_APPLY_AURA;
