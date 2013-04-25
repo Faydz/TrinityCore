9f89881852822ba7f4e565e1e93237dfc47f611c
Link88 <pof1981@hotmail.it>
2013-04-25 22:34:37 +0200

    [Db/Talents] Warrior, typo Incite

--- sql/forgottenlands/updates/world/2013_04_21_warrior_incite.sql
+++ sql/forgottenlands/updates/world/2013_04_21_warrior_incite.sql
@@ -1,4 +1,4 @@
-DELETE FROM `spell_proc_event` WHERE `entry` IN (50865, 50866, 50867);
-INSERT INTO `spell_proc_event` (`entry`, `SpellFamilyMask0`, `procEx`) VALUES (50865, 64, 2);
-INSERT INTO `spell_proc_event` (`entry`, `SpellFamilyMask0`, `procEx`) VALUES (50866, 64, 2);
-INSERT INTO `spell_proc_event` (`entry`, `SpellFamilyMask0`, `procEx`) VALUES (50867, 64, 2);
+DELETE FROM `spell_proc_event` WHERE `entry` IN (50685, 50686, 50687);
+INSERT INTO `spell_proc_event` (`entry`, `SpellFamilyMask0`, `procEx`) VALUES (50685, 64, 2);
+INSERT INTO `spell_proc_event` (`entry`, `SpellFamilyMask0`, `procEx`) VALUES (50686, 64, 2);
+INSERT INTO `spell_proc_event` (`entry`, `SpellFamilyMask0`, `procEx`) VALUES (50687, 64, 2);
