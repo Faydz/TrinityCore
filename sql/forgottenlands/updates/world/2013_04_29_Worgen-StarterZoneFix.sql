b9deaaa3d549db2b5d8597d179332fea16516c22
alex <alexsarpato@gmail.com>
2013-04-30 14:39:57 +0200

    DB: aggiustamenti ai worgen

--- sql/forgottenlands/updates/world/2013_04_29_Worgen-StarterZoneFix.sql
+++ sql/forgottenlands/updates/world/2013_04_29_Worgen-StarterZoneFix.sql
@@ -162,13 +162,14 @@ UPDATE `creature_template` SET `unit_class`=1, `Health_mod`=1020, `Mana_mod`=15 
 INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `cast_flags`) VALUES (35317, 46598, 1);
 
 -- Never surrender, Sometimes Retreat
-UPDATE `quest_template` SET `PrevQuestId`=14218 WHERE (`Id`=14221);
-UPDATE `quest_template` SET `RewardSpell`=59073 WHERE (`Id`=14221);
+UPDATE `quest_template` SET `PrevQuestId`=14218, `RewardSpell`= 0 WHERE (`Id`=14221);
 
 -- Last stand
+UPDATE `quest_template` SET `Flags`=0 WHERE (`Id`=14222);
+UPDATE `creature` SET `spawntimesecs`=120 WHERE (`id`=35627);
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE (`entry`=35566);
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 35566 AND `source_type` = 0;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
-(35566, 0, 0, 0, 20, 0, 100, 0, 14222, 0, 0, 0, 28, 59073, 0, 0, 0, 0, 0, 21, 10, 0, 0, 0, 0, 0, 0, 'Remove 1stPhase Worgen');
-(35566, 0, 1, 0, 20, 0, 100, 0, 14222, 0, 0, 0, 28, 72872, 0, 0, 0, 0, 0, 21, 10, 0, 0, 0, 0, 0, 0, 'Remove Infected Bite');
-(35566, 0, 2, 0, 20, 0, 100, 0, 14222, 0, 0, 0, 62, 0, 0, 0, 0, 0, 0, 21, 10, 0, 0, -8829, 626.3, 94.5, 3.95, 'Teleport to SW - Worgen');
+(35566, 0, 1, 0, 19, 0, 100, 0, 14222, 0, 0, 0, 75, 59073, 0, 0, 0, 0, 0, 21, 15, 0, 0, 0, 0, 0, 0, 'Add 1stPhase Worgen'),
+(35566, 0, 2, 0, 20, 0, 100, 0, 14222, 0, 0, 0, 28, 0, 0, 0, 0, 0, 0, 21, 15, 0, 0, 0, 0, 0, 0, 'Remove Auras'),
+(35566, 0, 3, 2, 20, 0, 100, 0, 14222, 0, 0, 0, 62, 0, 0, 0, 0, 0, 0, 21, 15, 0, 0, -8829, 626.3, 94.5, 3.95, 'Teleport to SW - Worgen');
