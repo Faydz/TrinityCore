3fdb9eca7afc19870a2d7ae593dcd2caf57967cc
Link88 <pof1981@hotmail.it>
2013-05-11 19:43:09 +0200

    Typo sql

--- sql/forgottenlands/updates/world/2013_05_11_warlock_soulburn_spells.sql
+++ sql/forgottenlands/updates/world/2013_05_11_warlock_soulburn_spells.sql
@@ -1,3 +1,4 @@
+DELETE FROM spell_linked_spell WHERE spell_trigger in(6262, 48020, 5676, 6353) AND spell_effect=-74434;
 INSERT INTO `spell_linked_spell` VALUES (6262, -74434, 0, 'Soulburn: Healthstone consumes buff');
 INSERT INTO `spell_linked_spell` VALUES (48020, -74434, 0, 'Soulburn: Demonic Circle: Teleport consumes buff');
 INSERT INTO `spell_linked_spell` VALUES (5676, -74434, 0, 'Soulburn: Searing Pain consumes buff');
