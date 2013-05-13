dc5c08e0384352872344bf78687be8fee4eee251
mik1893 <michele.roscelli@gmail.com>
2013-05-13 12:42:15 +0200

    [SQL/Spell] Oggi vengo castigato con i typo

--- sql/forgottenlands/updates/world/2013_05_13_Correct_Spell_Bonus_Data_Priest.sql
+++ sql/forgottenlands/updates/world/2013_05_13_Correct_Spell_Bonus_Data_Priest.sql
@@ -1,11 +1,19 @@
 DELETE FROM `spell_bonus_data` WHERE `entry` IN (15407, 589, 8092, 2944, 32379, 48045, 73510, 34914, 585);
 INSERT INTO `spell_bonus_data`  VALUES 
 (15407, 0, 0.864, 0, 0, 'priest- mind flay');
+INSERT INTO `spell_bonus_data`  VALUES 
 (589, 0, 0.966, 0, 0, 'priest- shadow word pain');
+INSERT INTO `spell_bonus_data`  VALUES 
 (8092, 1.104, 0, 0, 0, 'priest- mind blast');
+INSERT INTO `spell_bonus_data`  VALUES 
 (2944, 0, 1.304, 0, 0, 'priest- devouring plague');
+INSERT INTO `spell_bonus_data`  VALUES 
 (32379, 0.316, 0, 0, 0, 'priest- shadow word death');
+INSERT INTO `spell_bonus_data`  VALUES 
 (48045, 0, 1.311, 0, 0, 'priest- mind sear');
+INSERT INTO `spell_bonus_data`  VALUES 
 (73510, 0.836, 0, 0, 0, 'priest- mind spike');
+INSERT INTO `spell_bonus_data`  VALUES 
 (34914, 0, 1.76, 0, 0, 'priest- vampiric touch');
+INSERT INTO `spell_bonus_data`  VALUES 
 (585, 0.856, 0, 0, 0, 'priest- smite');
