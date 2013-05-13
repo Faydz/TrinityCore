cfce95840640872c1c5c34ddcd8136b022d9425a
mik1893 <michele.roscelli@gmail.com>
2013-05-13 12:40:06 +0200

    [SQL/Spell] Correzione coefficienti - SerpentSting - ImmoTrap - ExploTRap - BlackArrow- ExploShot

---
+++ sql/forgottenlands/updates/world/2013_05_13_correct_spell_bonus_data_hunter.sql
@@ -0,0 +1,7 @@
+DELETE FROM `spell_bonus_data` WHERE `entry` IN (1978, 13795, 13813, 3674, 53301);
+INSERT INTO `spell_bonus_data`  VALUES 
+(1978, 0, 0, 0, 0.08, 'hunter- serpent sting');
+(13795, 0, 0, 0, 0.02, 'hunter- immolation trap');
+(13813, 0, 0, 0, 0.055, 'hunter- explosive trap');
+(3674, 0, 0, 0, 0.133, 'hunter- black arrow');
+(53301, 0, 0, 0, 0.273, 'hunter- explosive shot');
