395db01069dfc316674ae9a9a7b6cd4a1fdc7bd9
Link88 <pof1981@hotmail.it>
2013-05-15 01:59:31 +0200

    [Core/Spells] Warlock , fix Soul Harvest health percent

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -6762,19 +6762,6 @@ void AuraEffect::HandlePeriodicHealAurasTick(Unit* target, Unit* caster) const
         
         switch (m_spellInfo->SpellFamilyName)
         {
-            case SPELLFAMILY_WARLOCK:
-                switch(m_spellInfo->Id)
-                {
-                    // Soul Harvest
-                    case 79268:
-                        {
-                            float tickPct = 45.0f / (float)GetTotalTicks();
-                        
-                            damage += CalculatePct(caster->GetMaxHealth(), tickPct);
-                        }
-                        break;
-                }
-                break;
             case SPELLFAMILY_WARRIOR:
                 switch (GetId())
                 {
