f383b20c824497436871c73324f354367d372cfc
Kepler <mattiag@anche.no>
2013-05-26 23:44:06 +0200

    [DB/Spell] Shaman, speed after thunderstorm reduced

---
+++ sql/forgottenlands/updates/world/2013_05_26_shaman__thudersorm_daze.sql
@@ -0,0 +1,2 @@
+DELETE FROM spell_linked_spell WHERE spell_trigger = 51490 AND spell_effect = 100955;
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (51490, 100955, 1, 'shaman thunderstorm dazed effect');
