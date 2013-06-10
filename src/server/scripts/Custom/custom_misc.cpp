16556158e830c55d9733f4b990eccc507f93c281
Karthaz <lukas-kleine@hotmail.de>
2013-06-10 21:44:11 +0200

    automatic spell learning

--- src/server/scripts/Custom/custom_misc.cpp
+++ src/server/scripts/Custom/custom_misc.cpp
@@ -5,33 +5,44 @@ void OnLevelChanged(Player * player, uint8 level)
 {
 	if (level == 20)
 	{
-		player->learnSpell(33388, true);
+		if (player->GetRace() == RACE_WORGEN)
+		{
+			player->learnSpell(68996);
+		}
+
+		player->learnSpell(33388);
+		player->SendSysMessage("Dies ist eine Kontrollnachricht");
 	}
 
 	if (level == 40)
 	{
-		player->learnSpell(33391, true);
+		player->learnSpell(33391);
+		player->SendSysMessage("Dies ist eine Kontrollnachricht");
 	}
 
 	if (level == 60)
 	{
-		player->learnSpell(34092, true);
+		player->learnSpell(34092);
+		player->SendSysMessage("Dies ist eine Kontrollnachricht");
 	}
 
 	if (level == 70)
 	{
-		player->learnSpell(90267, true);
-		player->learnSpell(34093, true);
+		player->learnSpell(90267);
+		player->learnSpell(34093);
+		player->SendSysMessage("Dies ist eine Kontrollnachricht");
 	}
 
 	if (level == 75)
 	{
-		player->learnSpell(54197, true);
+		player->learnSpell(54197);
+		player->SendSysMessage("Dies ist eine Kontrollnachricht");
 	}
 
 	if (level == 85)
 	{
-		player->learnSpell(90266, true);
+		player->learnSpell(90266);
+		player->SendSysMessage("Dies ist eine Kontrollnachricht");
 	}
 };
 
