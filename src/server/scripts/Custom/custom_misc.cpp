8669e008629b5b6d2f4b302fc24c87e63a0673d6
Karthaz <lukas-kleine@hotmail.de>
2013-06-10 21:57:42 +0200

    fix build

--- src/server/scripts/Custom/custom_misc.cpp
+++ src/server/scripts/Custom/custom_misc.cpp
@@ -5,44 +5,44 @@ void OnLevelChanged(Player * player, uint8 level)
 {
 	if (level == 20)
 	{
-		if (player->GetRace() == RACE_WORGEN)
+		if (player->getRace() == RACE_WORGEN)
 		{
-			player->learnSpell(68996);
+			player->learnSpell(68996, false);
 		}
 
-		player->learnSpell(33388);
-		player->SendSysMessage("Dies ist eine Kontrollnachricht");
+		player->learnSpell(33388, false);
+		player->SetMoney(80000);
 	}
 
 	if (level == 40)
 	{
-		player->learnSpell(33391);
-		player->SendSysMessage("Dies ist eine Kontrollnachricht");
+		player->learnSpell(33391, false);
+		player->SetMoney(80000);
 	}
 
 	if (level == 60)
 	{
-		player->learnSpell(34092);
-		player->SendSysMessage("Dies ist eine Kontrollnachricht");
+		player->learnSpell(34092, false);
+		player->SetMoney(80000);
 	}
 
 	if (level == 70)
 	{
-		player->learnSpell(90267);
-		player->learnSpell(34093);
-		player->SendSysMessage("Dies ist eine Kontrollnachricht");
+		player->learnSpell(90267, false);
+		player->learnSpell(34093, false);
+		player->SetMoney(80000);
 	}
 
 	if (level == 75)
 	{
-		player->learnSpell(54197);
-		player->SendSysMessage("Dies ist eine Kontrollnachricht");
+		player->learnSpell(54197, false);
+		player->SetMoney(80000);
 	}
 
 	if (level == 85)
 	{
-		player->learnSpell(90266);
-		player->SendSysMessage("Dies ist eine Kontrollnachricht");
+		player->learnSpell(90266, false);
+		player->SetMoney(80000);
 	}
 };
 
