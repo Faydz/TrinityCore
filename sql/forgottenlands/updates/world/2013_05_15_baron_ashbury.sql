8d4a2420cc00bf7d1e71ed0a532843338cf1498e
Kazlim <marco@formadigitale.it>
2013-06-19 12:34:33 +0200

    [DB/Scripts] Baron Ashbury SQL correction

--- sql/forgottenlands/updates/world/2013_05_15_baron_ashbury.sql
+++ sql/forgottenlands/updates/world/2013_05_15_baron_ashbury.sql
@@ -1,4 +1,5 @@
-INSERT IGNORE INTO `creature` VALUES (NULL, 46962, 33, 2, 1, 0, 0, -256.12, 2117.15, 81.2629, 1.23918, 7200, 0, 0, 1, 0, 0, 0, 0, 0);
+DELETE FROM `creature` WHERE `id`= 46962 AND `spawnMask`=2;
+INSERT INTO `creature` VALUES (NULL, 46962, 33, 2, 1, 0, 0, -256.12, 2117.15, 81.2629, 1.23918, 7200, 0, 0, 1, 0, 0, 0, 0, 0);
 
 UPDATE `creature_template` SET `difficulty_entry_1`=49708, `ScriptName`='boss_baron_ashbury' WHERE (`entry`=46962);
 UPDATE `creature_template` SET `faction_A`=14, `faction_H`=14, `unit_class`=2, `unit_flags`=32832 WHERE (`entry`=49708);
