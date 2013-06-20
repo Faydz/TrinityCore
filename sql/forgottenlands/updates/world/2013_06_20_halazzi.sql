c4a64b0446495a4c3262648d55e4dfc9d6dfae13
Link88 <pof1981@hotmail.it>
2013-06-20 20:05:17 +0200

    [SQL] Corrected Halazzi sql

--- sql/forgottenlands/updates/world/2013_06_20_halazzi.sql
+++ sql/forgottenlands/updates/world/2013_06_20_halazzi.sql
@@ -2,7 +2,7 @@
 UPDATE `creature_template` SET `unit_flags`=4, `Health_mod`=2381, `faction_A`=1890, `faction_H`=1890, `spell1`=97502 WHERE (`entry`=52755);
  
 -- Spirit Lynx
-UPDATE `creature_template` SET `Health_mod`=273.4, SET `mechanic_immune_mask`=0 WHERE (`entry`=24143);
+UPDATE `creature_template` SET `Health_mod`=273.4, `mechanic_immune_mask`=0 WHERE (`entry`=24143);
 
 -- Lightning Totem
 UPDATE `creature_template` SET `unit_flags`=4, `faction_A`=1890, `faction_H`=1890, `Health_mod`=5.69 WHERE (`entry`=24224);
