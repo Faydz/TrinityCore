8bf041d30744589bec0ffa556b70990be4b30134
dimiandre <dimiandre@gmail.com>
2013-06-24 20:24:59 +0200

    Another sql typo, merge

--- sql/forgottenlands/updates/world/2013_24_06_bwd_adjust.sql
+++ sql/forgottenlands/updates/world/2013_24_06_bwd_adjust.sql
@@ -1,3 +1,3 @@
-DELETE FROM`creature_template_addon` WHERE `entry` I N(42920,42934);
+DELETE FROM`creature_template_addon` WHERE `entry` IN (42920,42934);
 INSERT INTO `creature_template_addon` (`entry`, `auras`) VALUES (42934, '80162');
 INSERT INTO `creature_template_addon` (`entry`, `auras`) VALUES (42920, '80095');
