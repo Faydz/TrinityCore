d7dd5b0bcea739b411f7e6a2db9ea1065d53b771
Kazlim <marco@formadigitale.it>
2013-06-25 02:23:27 +0200

    Typo SQL

--- sql/forgottenlands/updates/world/2013_06_24_arena_spectator.sql
+++ sql/forgottenlands/updates/world/2013_06_24_arena_spectator.sql
@@ -1,8 +1,8 @@
 DELETE FROM `command` WHERE `name` = 'spectate';        
 INSERT INTO `command` (`name`, `security`, `help`) VALUES ('spectate', 0, 'Syntax: .spectate $subcommand.\nUse .help sppectate');
-DELETE FROM `command` WHERE `name` = 'spectatefrom';    
+DELETE FROM `command` WHERE `name` = 'spectate view';    
 INSERT INTO `command` (`name`, `security`, `help`) VALUES ('spectate view', 0, 'Syntax: .spectate view #player\nAllow player to spectate arena from anotherplayer.');
-DELETE FROM `command` WHERE `name` = 'spectate leav';   
+DELETE FROM `command` WHERE `name` = 'spectate leave';   
 INSERT INTO `command` (`name`, `security`, `help`) VALUES ('spectate leave', 0, 'Syntax: .spectate leave\nDisable spectator mode.');
 DELETE FROM `command` WHERE `name` = 'spectate player'; 
 INSERT INTO `command` (`name`, `security`, `help`) VALUES ('spectate player', 0, 'Syntax: .spectate player #player\nAllow to spectate player.');
