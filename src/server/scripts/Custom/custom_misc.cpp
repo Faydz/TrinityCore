ffe6b84f448a84f920bbe04975294b8097faacd5
Karthaz <lukas-kleine@hotmail.de>
2013-06-26 20:03:33 +0200

    Damned Alzheimer

--- src/server/scripts/Custom/custom_misc.cpp
+++ src/server/scripts/Custom/custom_misc.cpp
@@ -18,38 +18,32 @@ LearnSpellsOnLevelUp() : PlayerScript("LearnSpellsOnLevelUp")
 			}
 
 			player->learnSpell(33388, false);
-			player->SetMoney(80000);
 		}
 
 		if (leveld >= 40)
 		{
 			player->learnSpell(33391, false);
-			player->SetMoney(80000);
 		}
 
 		if (leveld >= 60)
 		{
 			player->learnSpell(34092, false);
-			player->SetMoney(80000);
 		}
 
 		if (leveld >= 70)
 		{
 			player->learnSpell(90267, false);
 			player->learnSpell(34093, false);
-			player->SetMoney(80000);
 		}
 
 		if (leveld >= 75)
 		{
 			player->learnSpell(54197, false);
-			player->SetMoney(80000);
 		}
 
 		if (leveld >= 85)
 		{
 			player->learnSpell(90266, false);
-			player->SetMoney(80000);
 		}
 	};
 };
