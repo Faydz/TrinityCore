c94c264a4acb615bf4112476227a8fdb15111f6f
Link88 <pof1981@hotmail.it>
2013-06-30 18:23:25 +0200

    [Core/Misc] Fix arena rating gain for wins with huge victory chance

--- src/server/game/Battlegrounds/ArenaTeam.cpp
+++ src/server/game/Battlegrounds/ArenaTeam.cpp
@@ -667,6 +667,8 @@ int32 ArenaTeam::GetRatingMod(uint32 ownRating, uint32 ownMMRating, uint32 oppon
         {
             mod = 24.0f * change;
         }
+
+        mod = mod < 1.0f ? 1.0f : mod;
     }
     // Loser
     else
