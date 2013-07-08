d9c2f0c8d37c06b0209d2f8ac77f5843418a0b90
Link88 <pof1981@hotmail.it>
2013-07-08 21:17:23 +0200

    [Db/Talents] Hunter, fix The Beast Within PvP trinket effect on activation

---
+++ sql/forgottenlands/updates/world/2013_07_08_hun_the_beast_within.sql
@@ -0,0 +1,2 @@
+DELETE FROM spell_linked_spell WHERE spell_trigger = 34471 AND spell_effect = 70029;
+INSERT INTO `spell_linked_spell` VALUES (34471, 70029, 0, 'Hunter - The Beast Within PvP trinket effect');
