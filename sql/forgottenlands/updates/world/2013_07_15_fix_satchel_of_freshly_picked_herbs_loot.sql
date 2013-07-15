fb69b76b40845b9e2c3c27b70cdb6d7d3fd24295
Naaroth <pinonasty@hotmail.it>
2013-07-15 02:40:15 +0200

    [Database/item_loot_template] typo in last commit

--- sql/forgottenlands/updates/world/2013_07_15_fix_satchel_of_freshly_picked_herbs_loot.sql
+++ sql/forgottenlands/updates/world/2013_07_15_fix_satchel_of_freshly_picked_herbs_loot.sql
@@ -5,5 +5,5 @@ INSERT IGNORE INTO  `reference_loot_template` (`entry`, `item`, `ChanceOrQuestCh
 (10036, 52988, 0, 1, 1, 5, 5),
 (10036, 52985, 0, 1, 1, 5, 5),
 (10036, 52984, 0, 1, 1, 5, 5);
-INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES 
+INSERT IGNORE INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES 
 (68813, 0, 100, 1, 0, -10036, 4);
