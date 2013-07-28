0615de852b3f927378651d93659c6e9dda7eaaa6
Kazlim <marco@formadigitale.it>
2013-07-28 23:29:00 +0200

    typo

--- sql/forgottenlands/updates/world/2013_07_28_boss_akilzon.sql
+++ sql/forgottenlands/updates/world/2013_07_28_boss_akilzon.sql
@@ -2,6 +2,5 @@ UPDATE `creature_template` SET `faction_A`=14, `faction_H`=14 WHERE (`entry`=526
 
 UPDATE `creature_template` SET `ScriptName`='npc_amani_kidnapper' WHERE (`entry`=52638);
 
-
 UPDATE `creature_template` SET `VehicleId`=1598 WHERE (`entry`=52638);
 UPDATE `creature_template` SET `VehicleId`=1598 WHERE (`entry`=52639);
