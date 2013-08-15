640552cc7585f454cb670d864c21b58f985146b4
Karthaz <lukas-kleine@hotmail.de>
2013-08-15 21:09:49 +0200

    Fix build.

--- src/server/scripts/EasternKingdoms/BastionOfTwilight/boss_halfus.cpp
+++ src/server/scripts/EasternKingdoms/BastionOfTwilight/boss_halfus.cpp
@@ -91,10 +91,10 @@
      
      
                     };
-				ScriptedAI* GetAI (Creature* creature) const
-				{
-					return new boss_creatureAI(creature);
-				}
+				//ScriptedAI* GetAI (Creature* creature) const
+				//{
+				//	return new boss_creatureAI(creature);
+				//}
 
     };
      
