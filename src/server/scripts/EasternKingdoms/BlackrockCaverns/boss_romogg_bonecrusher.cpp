ce3fd3e79540be81282ef09ae601c55247d4c4cf
Karthaz <lukas-kleine@hotmail.de>
2013-08-24 20:23:25 +0200

    Change Name

--- src/server/scripts/EasternKingdoms/BlackrockCaverns/boss_romogg_bonecrusher.cpp
+++ src/server/scripts/EasternKingdoms/BlackrockCaverns/boss_romogg_bonecrusher.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2011 - 2012 Forgotten Core <http://www.forgottenlands.eu/>
+* Copyright (C) 2011 - 2012 DarkThunder Core <http://darkthunder-wow.net/>
 *
 * This program is free software; you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
