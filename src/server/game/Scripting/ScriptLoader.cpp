bf7f8fe01d0a2680704314c4116af052d6893b3e
Karthaz <lukas-kleine@hotmail.de>
2013-08-27 21:49:33 +0200

    Revert "Added GrimBatol Scripts"
    This reverts commit c7d9b9e9684b870cf7d53d7f468fd48e19a69e49.

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -696,8 +696,6 @@ void AddSC_chat_log();
 // arena watcher
 void AddSC_arena_watcher();
 void AddSC_custom_misc();
-void AddSC_Generall_Umbriss();
-void AddSC_boss_Forgemaster_Throngus();
 #endif
 
 void AddScripts()
@@ -1428,7 +1426,5 @@ void AddCustomScripts()
 #ifdef SCRIPTS
     AddSC_arena_watcher();
 	AddSC_custom_misc();
-	AddSC_Generall_Umbriss();
-	AddSC_boss_Forgemaster_Throngus();
 #endif
 }
